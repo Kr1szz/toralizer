1e7dab639bca1fa804494d1819e88c999332f0c5
kriszz <kriszzgfx@gmail.com>
2026-05-06 23:54:00 +0530

    feat:  core development of libraries and implementaton of SOCKS5

--- toralize.c
+++ toralize.c
@@ -61,3 +61,109 @@ int socks5_authenticate(int sockfd) {
 
   return 0;
 }
+
+
+/* SOCKS5 Connection Request*/
+int socks5_connect(int sockfd, const struct sockaddr_in *addr) {
+  unsigned char buffr[BUFFER_SIZE];
+  unsigned char reply[BUFFER_SIZE];
+  unsigned char address[0];
+  int len = 0;
+
+  // socks5 handshake
+  buffr[0] = SOCKS5_VERSION;
+  buffr[1] = SOCKS5_CONNECT;
+  // Number of authentication methods
+
+  if (send(sockfd, buffr, 3, 0) < 0)
+    return -1;
+
+  if (recv(sockfd, &reply[0], 1, 0) < 0)
+    return -1;
+
+  if (reply[0] != 0x00)
+    return -1;
+
+  // Build connect request
+  buffr[0] = SOCKS5_VERSION;
+  buffr[1] = SOCKS5_CONNECT;
+  buffr[2] = 0;
+  buffr[3] = SOCKS5_IPV4;
+  // copy ip addresses
+
+  memcpy(&buffr[4], &addr->sin_addr, 4);
+  buffr[8] = htons(addr->sin_port) & 0xFF;
+  buffr[9] = htons(addr->sin_port) >>8 ;
+     // Send connect request
+    if (send(sockfd, buffr, 10, 0) < 0) 
+        return -1;
+    
+    
+    // Receive reply
+    if (recv(sockfd, &reply[0], 1, 0) < 0) 
+        return -1;
+    
+    
+    if (reply[0] != 0x00) 
+        return -1;
+
+  return 0;
+}
+
+
+// Override connect function
+int connect(int sockfd, const struct sockaddr *addr, socklen_t len) {
+    // If this is a connection to Tor proxy, use real connect
+    if (is_tor_proxy_address(addr, len)) {
+        return real_connect(sockfd, addr, len);
+    }
+    
+    // Create socket to Tor proxy
+    int tor_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (tor_fd < 0) {
+        return -1;
+    }
+    
+    struct sockaddr_in tor_addr;
+    memset(&tor_addr, 0, sizeof(tor_addr));
+    tor_addr.sin_family = AF_INET;
+    tor_addr.sin_port = htons(TOR_PROXY_PORT);
+    
+    if (inet_pton(AF_INET, TOR_PROXY_HOST, &tor_addr.sin_addr) <= 0) {
+        close(tor_fd);
+        return -1;
+    }
+    
+    // Connect to Tor proxy
+    if (real_connect(tor_fd, (struct sockaddr *)&tor_addr, sizeof(tor_addr)) < 0) {
+        close(tor_fd);
+        return -1;
+    }
+    
+    // Perform SOCKS5 authentication
+    if (socks5_authenticate(tor_fd) < 0) {
+        close(tor_fd);
+        return -1;
+    }
+    
+    // Send SOCKS5 connect request
+    if (socks5_connect(tor_fd, (struct sockaddr_in *)addr) < 0) {
+        close(tor_fd);
+        return -1;
+    }
+    
+    // Close Tor proxy connection as SOCKS5 handshake is complete
+    close(tor_fd);
+    
+    return 0;
+}
+// Initialization function
+__attribute__((constructor))
+void init_toralizer() {
+    // Get real connect function
+    real_connect = dlsym(RTLD_NEXT, "connect");
+    if (!real_connect) {
+        fprintf(stderr, "Error: Could not find real connect function\n");
+        exit(1);
+    }
+}
